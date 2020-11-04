c73b4e04b0e02574b7c870071a2c09c930d48d9b
Jerome-LorD <jerome-divry@laposte.net>
2020-11-04 10:35:33 +0100

    Little refactoring

--- views/cliview.py
+++ views/cliview.py
@@ -17,52 +17,53 @@ class CLIView:
         }
 
     def display_items_taken(self):
-        """Display items and texts"""
+        """Display items taken and texts"""
         items = [
             item
             for item, pos in self.lab.items_position.items()
             if not self.lab.items_position[item]
         ]
-        print(f"In your possession -> {' '.join(items)}")
+        if items:
+            print(f"{settings.POSSESSION} {' '.join(items)}")
 
         if (
             self.lab.collision_counter == 3
             and not self.lab.player.pos == self.lab.finish[0]
         ):
             print(f"{settings.GOT_SYRINGE} {settings.SYRINGE_UNICODE}")
-        if (
-            self.lab.collision_counter < 3
-            and self.lab.player.pos == self.lab.finish[0]
-        ):
-            print(settings.LOSECLI)        
-            self.lab.run = False
-        # if self.lab.lose == True:
+        if self.lab.collision_counter < 3\
+                and self.lab.player.pos == self.lab.finish[0]:
+            print(settings.LOSECLI)
+            self.lab.run_state = 1
 
-            
         if (
             self.lab.collision_counter == 3
             and self.lab.player.pos == self.lab.finish[0]
         ):
-            print(settings.WINCLI)        
-            self.lab.run = False
-
+            print(f"{settings.WINCLI} {settings.SYRINGE_UNICODE}")
+            self.lab.run_state = 1
 
         if (
             self.lab.collision_counter < 3
             and not self.lab.player.pos == self.lab.finish[0]
+            and self.lab.run_state == 0
         ):
-            print("take all objects if you wanna get out.")
+            print(settings.ADVICE)
 
     def header(self):
-        self.dashes = "# ------------------------- #"
+        """Display header"""
         print(
-            f"\n{self.dashes}\n#-MacGyver -- Get out vc-1 -#\n\
-{self.dashes}"
+            f"\n{settings.DASHES}\n{settings.HEADER}\n\
+{settings.DASHES}"
         )
 
     def display(self):
         """Display the game"""
         self.header()
+
+        reversed_items_position = {
+            pos: name for name, pos in self.lab.items_position.items()
+        }
 
         for pos_y in range(settings.WIDTH):
             for pos_x in range(settings.HEIGHT):
@@ -70,15 +71,11 @@ class CLIView:
 
                 if position == self.lab.player.pos:
                     self.pic = settings.PLAYER_CHAR
-                elif position == self.lab.items_position["tube"]:
-                    self.pic = settings.TUBE_CHAR
-                elif position == self.lab.items_position["needle"]:
-                    self.pic = settings.NEEDLE_CHAR
-                elif position == self.lab.items_position["ether"]:
-                    self.pic = settings.ETHER_CHAR
-                elif position in self.lab.wall:
+                elif position in self.lab.items_position.values():
+                    self.pic = self.images[reversed_items_position[position]]
+                elif position in self.lab.walls:
                     self.pic = "X"
-                elif position == self.lab.start:
+                elif position == self.lab.starts:
                     self.pic = "S"
                 elif position in self.lab.finish:
                     self.pic = "F"

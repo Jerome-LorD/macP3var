d5302a3570043f70274d492b48e33382e728e98e
Jerome-LorD <jerome-divry@laposte.net>
2020-11-04 10:28:04 +0100

    add settings

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 """Main application."""
 
+import settings
 from app import Application
+
 
 def main():
     """Entry point of the Maze application."""
     choices = {"1": "cli", "2": "pygame"}
-    choice = input("Type a choice:\n1. cli\n2. pygame - [1 or 2]-> ")
+    choice = input(settings.CHOICE)
 
     if choice not in choices:
         choice = "1"

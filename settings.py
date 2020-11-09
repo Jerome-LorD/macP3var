f6d61b6c73a355692939da1913f0b136cc02f571
Jerome-LorD <jerome-divry@laposte.net>
2020-11-09 16:04:09 +0100

    Add colorama init and replacements

--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
+"""Contains all constants."""
+
+from colorama import init
 from termcolor import colored
 from pathlib import Path
 
+init(autoreset=True)
 
 BASE_DIR = Path(".")
 DATA_DIR = BASE_DIR / "data"
@@ -21,28 +25,28 @@ TEXT_POSITION = (30, 610)
 WIDTH = 15
 HEIGHT = 15
 
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-GREEN = (0,255,0)
-RED = (255,0,0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
-WALL_RECT = (340,100,20,20)
-STARTPOL_RECT = (160,20,20,20)
-PLASTIC_TUBE_RECT = (0,0,259,194)
-ETHER_RECT = (0,0,225,225)
-NEEDLE_RECT = (0,0,545,720)
-SYRINGE_RECT = (0,0,90,90)
-FRM_BOX = 0,603,600,53
-STANDARD_IMG_RECT = (0,0,40,40)
+WALL_RECT = (340, 100, 20, 20)
+STARTPOL_RECT = (160, 20, 20, 20)
+PLASTIC_TUBE_RECT = (0, 0, 259, 194)
+ETHER_RECT = (0, 0, 225, 225)
+NEEDLE_RECT = (0, 0, 545, 720)
+SYRINGE_RECT = (0, 0, 90, 90)
+FRM_BOX = 0, 603, 600, 53
+STANDARD_IMG_RECT = (0, 0, 40, 40)
 
 FRAME_WIDTH = 10
-ETHER_POS = (455,610)
-NEEDLE_POS = (500,610)
-TUBE_POS = (545,610)
-SYRINGE_POS = (410,610)
+ETHER_POS = (455, 610)
+NEEDLE_POS = (500, 610)
+TUBE_POS = (545, 610)
+SYRINGE_POS = (410, 610)
+IMG_WIN_POS = (120, 120)
 
 IMG_SIZE = 40
-IMG_HIDE_SIZE = (0, 0)
 
 START_CHAR = "s"
 WALL_CHAR = "W"
@@ -59,9 +63,9 @@ INPUT = "Type the direction [right, left, up, down (q to quit)]-> "
 NOT_IN_BOARD = "Command not in the board"
 HEADER = f'#{colored("-MacGyver -- Get out vc-1 -", "yellow")}#'
 DASHES = "# ------------------------- #"
-ADVICE = "take all objects if you wanna get out."
+ADVICE = "Take all items if you wanna get out."
 POSSESSION = "In your possession -> "
 WINCLI = colored("Congats, You WIN", "green")
 LOSECLI = colored("You lose, you die\nGAME OVER", "red")
 GOT_SYRINGE = "You got the syringe, you can get out -> "
-THX = colored("thx, until next time", "green")
+THX = colored("Thx, until next time", "green")

eaf8bf0d0a4289eac3d87f987984c2b17baef7b2
Jerome-LorD <jerome-divry@laposte.net>
2020-11-09 15:43:03 +0100

    refactoring, remove init

--- controllers/clicontroller.py
+++ controllers/clicontroller.py
@@ -1,27 +1,21 @@
+"""Clicontroller module transmits commands to the player module."""
+
+
 import settings
 
 
 class CLIController:
-    """Handle controls from keyboard inputs"""
+    """Handle controls from keyboard inputs."""
 
-    def __init__(self, labyrinth):
-        self.lab = labyrinth
+    def handle_control(self) -> str:
+        """Manage and return command from input."""
+        command = ""
+        board = ["up", "right", "down", "left", "q"]
 
-    def handle_control(self):
-        """Manage all direction from input"""
+        command = input(settings.INPUT)
 
-        if self.lab.run_state != 0:
-            return "q"
-        else:
-            command = ""
-            board = ["up", "right", "down", "left", "q"]
-
-            command = str(
-                input(settings.INPUT)
-            )
-
-            if command == "q":
-                print(f"\n{settings.THX}")
-            if command not in board:
-                print(settings.NOT_IN_BOARD)
-            return command
+        if command == "q":
+            print(f"\n{settings.THX}")
+        if command not in board:
+            print(settings.NOT_IN_BOARD)
+        return command

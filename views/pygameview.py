faed0c2606a09f7edf3c429aef0c53e80ac6ffa4
Jerome-LorD <jerome-divry@laposte.net>
2020-11-09 16:10:07 +0100

    Refactoring and add typing

--- views/pygameview.py
+++ views/pygameview.py
@@ -1,35 +1,41 @@
+"""pygameview load all graphical parts and display the game onto a pygame screen."""
+
 import pygame as pg
 
+from typing import Dict, Tuple, Any
+
+from model.labyrinth import Labyrinth
 import settings
 
 
 class PYView:
-    """Manage and display images and texts with pygame"""
+    """Manage and display images and texts with pygame."""
 
     def __init__(self, lab):
+        """Init."""
         pg.init()
-        self.lab = lab
+        self.lab: Labyrinth = lab
 
-        self.items_pos = {
+        self.items_pos: Dict[str, tuple] = {
             "tube": settings.TUBE_POS,
             "needle": settings.NEEDLE_POS,
             "ether": settings.ETHER_POS,
         }
 
-        self.screen_size = settings.SCREEN_SIZE
+        self.screen_size: Tuple[int, int] = settings.SCREEN_SIZE
         self.screen = pg.display.set_mode(self.screen_size)
         pg.display.set_caption("MacGyver - V1.0")
 
-        self.text = [
-            "Make a syringe.",
-            "Nice job! Now, you can get out.",
-            "You lose! you die... GAME OVER !!!",
-            "Congrats, you win! Free to get out",
-        ]
+        self.text: Dict[str, str] = {
+            "advice": "Make a syringe.",
+            "nice": "Nice job! Now, you can get out.",
+            "lose": "You lose! you die... GAME OVER !!!",
+            "win": "Congrats, you win! Free to get out",
+        }
 
         self.font = self.load_font(settings.FONT_FILE)
 
-        self.images = {
+        self.images: Dict[str, Any] = {
             "macgyver": self.load_image(settings.IMG_DIR / "macgyver.png"),
             "macgyver2": self.load_image(settings.IMG_DIR / "macgyver2.png"),
             "tube": self.load_image(settings.IMG_DIR / "tube2.png"),
@@ -42,90 +48,102 @@ class PYView:
             "ground": self.load_image(settings.IMG_DIR / "ground.png"),
         }
 
-    def load_font(self, path):
+    def load_font(self, path: str) -> pg.font:
+        """Load font."""
         try:
             return pg.font.Font(str(path), settings.FONT_SIZE)
         except FileNotFoundError:
             raise SystemExit(f"Cannot load font: {path}")
 
-    def load_image(self, path):
+    def load_image(self, path: str) -> pg.Surface:
+        """Load image."""
         try:
             return pg.image.load(str(path)).convert_alpha()
         except FileNotFoundError:
             raise SystemExit(f"Cannot load image: {path}")
 
-    def display_items(self):
-        """Display items in the bottom frame"""
-        for name in self.items_pos:
-            if not self.lab.items_position[name]:
-                self.screen.blit(self.images[name], self.items_pos[name])
-            if self.lab.collision_counter == 3:
-                self.screen.blit(self.images["syringe"], settings.SYRINGE_POS)
-        if (
-            self.lab.collision_counter == 3
-            and self.lab.player.pos == self.lab.finish[0]
-        ):
-            self.screen.blit(self.images["macgyver2"], (120, 120))
+    def display_items_in_frame(self):
+        """Display items in the bottom frame."""
+        for item in self.lab.player.bag:
+            self.screen.blit(self.images[item.name], self.items_pos[item.name])
 
-    def display_txt(self):
-        """Display texts in the bottom frame"""
-        if self.lab.collision_counter < 3\
-                and self.lab.player.pos == self.lab.finish[0]:
+    def display_syringe(self):
+        """Display syringe if player get all items."""
+        if len(self.lab.player.bag) == len(self.lab.items_names):
+            self.screen.blit(self.images["syringe"], settings.SYRINGE_POS)
+
+    def display_win(self):
+        """Display win image."""
+        if (
+            len(self.lab.player.bag) == len(self.lab.items_names)
+            and self.lab.player.pos == self.lab.finish
+        ):
+            self.screen.blit(self.images["macgyver2"], settings.IMG_WIN_POS)
+
+    def display_text(self):
+        """Display texts in the bottom frame."""
+        if (
+            len(self.lab.player.bag) < len(self.lab.items_names)
+            and self.lab.player.pos == self.lab.finish
+        ):
             self.lab.run_state = 1
             return self.font.render(
-                self.text[2], True, settings.RED, settings.BLACK
-                )
+                self.text["lose"], True, settings.RED, settings.BLACK
+            )
         elif (
-            self.lab.collision_counter == 3
-            and not self.lab.player.pos == self.lab.finish[0]
+            len(self.lab.player.bag) == len(self.lab.items_names)
+            and not self.lab.player.pos == self.lab.finish
         ):
             return self.font.render(
-                self.text[1], True, settings.WHITE, settings.BLACK
-                )
+                self.text["nice"], True, settings.WHITE, settings.BLACK
+            )
         elif (
-            self.lab.collision_counter == 3
-            and self.lab.player.pos == self.lab.finish[0]
+            len(self.lab.player.bag) == len(self.lab.items_names)
+            and self.lab.player.pos == self.lab.finish
         ):
             self.lab.run_state = 1
             return self.font.render(
-                self.text[3], True, settings.GREEN, settings.BLACK
-                )
+                self.text["win"], True, settings.GREEN, settings.BLACK
+            )
         else:
             return self.font.render(
-                self.text[0], True, settings.WHITE, settings.BLACK
-                )
+                self.text["advice"], True, settings.WHITE, settings.BLACK
+            )
+
+    def display_items(self):
+        """Display items in the game."""
+        for item in self.lab.items:
+            pos_x, pos_y = item.coords
+            position = pos_x * settings.IMG_SIZE, pos_y * settings.IMG_SIZE
+
+            self.screen.blit(self.images[item.name], position)
 
     def display(self):
-        """Display the game"""
-
-        reversed_items_position = {
-            pos: name for name, pos in self.lab.items_position.items()
-        }
-
+        """Display the game."""
         pg.draw.rect(
             self.screen, settings.WHITE, settings.FRM_BOX, settings.FRAME_WIDTH
-            )
-        self.screen.blit(self.display_txt(), settings.TEXT_POSITION)
+        )
+        self.screen.blit(self.display_text(), settings.TEXT_POSITION)
 
         for pos_y in range(settings.WIDTH):
             for pos_x in range(settings.HEIGHT):
                 position = pos_x, pos_y
-                pygame_pos =\
-                    pos_x * settings.IMG_SIZE, pos_y * settings.IMG_SIZE
+                pygame_pos = pos_x * settings.IMG_SIZE, pos_y * settings.IMG_SIZE
 
                 if position == self.lab.player.pos:
                     surface = self.images["macgyver"]
-                elif position in self.lab.items_position.values():
-                    surface = self.images[reversed_items_position[position]]
                 elif position in self.lab.walls:
                     surface = self.images["brick"]
                 elif position == self.lab.starts:
                     surface = self.images["start"]
-                elif position in self.lab.finish:
+                elif position == self.lab.finish:
                     surface = self.images["gatekeeper"]
                 elif position in self.lab.roads:
                     surface = self.images["ground"]
 
                 self.screen.blit(surface, pygame_pos)
                 self.display_items()
+                self.display_items_in_frame()
+                self.display_syringe()
+                self.display_win()
                 pg.display.flip()
